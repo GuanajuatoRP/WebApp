a8a4458cfb20a573cf61b04c1ce4a982a368d76b
Dercraker <antoine.capitain@gmail.com>
2022-06-14 16:06:06 +0200

    Page Sessions v1 finie

--- guanajuato-rp/src/api/UserAPI.ts
+++ guanajuato-rp/src/api/UserAPI.ts
@@ -1,14 +1,6 @@
 import BaseApi from '@/api/BaseApi';
-import { LoginModel } from '@/models/Auth/LoginModel';
 
 export class UserAPI {
-  static async login(username: string, password: string) {
-    const loginInfo = new LoginModel();
-    loginInfo.username = username;
-    loginInfo.password = password;
-    return await BaseApi.AppAnonymous.post('/Login', loginInfo);
-  }
-
   static async getCarouselImage() {
     const response = await BaseApi.AppAnonymous.get('api/Web/CarouselImages');
     return response.data;
