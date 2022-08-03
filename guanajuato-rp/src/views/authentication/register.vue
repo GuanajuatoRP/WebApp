f3cdf23f8486f1724baf062e12ec7ca8e1861f68
Dercraker <antoine.capitain@gmail.com>
2022-08-03 17:44:57 +0200

    gitflow-feature-stash: register*

--- guanajuato-rp/src/views/authentication/register.vue
+++ guanajuato-rp/src/views/authentication/register.vue
@@ -1,5 +1,31 @@
 <template>
-  <v-container fluid> Register </v-container>
+  <v-container fluid>
+    <template>
+      <v-card>
+        <v-card-title>Login Form</v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              label="Email"
+              v-model="email"
+              :rules="[(v) => !!v || 'Email is required']"
+              required
+            ></v-text-field>
+            <v-text-field
+              label="Password"
+              v-model="password"
+              type="password"
+              :rules="[(v) => !!v || 'Password is required']"
+              required
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-container>
 </template>
 
 <script lang="ts">
