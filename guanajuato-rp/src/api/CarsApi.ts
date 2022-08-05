138f2a662c6fd348f9902facdb295d4d1a1e4840
kilarn123 <kilarn123@gmail.com>
2022-08-05 16:10:52 +0200

    ajout recherche voiture concessionaire

--- guanajuato-rp/src/api/CarsApi.ts
+++ guanajuato-rp/src/api/CarsApi.ts
@@ -2,15 +2,22 @@ import BaseApi from '@/api/BaseApi';
 
 export class CarsApi {
   static async getListMakers() {
-    const response = await BaseApi.CarsApi.get('api/Infos/Makers');
+    const response = await BaseApi.AppLogged.get('api/Infos/Makers');
     return response.data;
   }
   static async getListTypes() {
-    const response = await BaseApi.CarsApi.get('api/Infos/Types');
+    const response = await BaseApi.AppLogged.get('api/Infos/Types');
     return response.data;
   }
   static async getListPays() {
-    const response = await BaseApi.CarsApi.get('api/Infos/Pays');
+    const response = await BaseApi.AppLogged.get('api/Infos/Pays');
+    return response.data;
+  }
+
+  static async search(marque: string, pays: string, type: string, modele: string) {
+    const response = await BaseApi.AppLogged.get(
+      `api/OriginalCar/Search?marque=${marque}&pays=${pays}&type=${type}&modele=${modele}`
+    );
     return response.data;
   }
 }
--- guanajuato-rp/src/views/entreprises/concessionnaire.vue
+++ guanajuato-rp/src/views/entreprises/concessionnaire.vue
@@ -1,84 +1,198 @@
 <template>
   <v-container fluid>
-    <v-row>
-      <v-col cols="12" sm="6" md="3" pa-2>
-        <v-combobox v-model="selectedMaker" :items="makers" label="Marque" :loading="makerLoading" outlined ma-1>
+    <v-row no-gutters>
+      <v-col cols="12" sm="6" md="3" class="pa-2">
+        <v-combobox
+          v-model="selectedMaker"
+          :items="makers"
+          label="Marque"
+          :loading="makerLoading"
+          outlined
+          clearable
+          hide-details="auto"
+        >
         </v-combobox>
       </v-col>
-      <v-col cols="12" sm="6" md="3" pa-2>
-        <v-combobox v-model="selectedPays" :items="pays" label="Pays" :loading="paysLoading" outlined ma-1>
+      <v-col cols="12" sm="6" md="3" class="pa-2">
+        <v-combobox
+          v-model="selectedPays"
+          :items="pays"
+          label="Pays"
+          :loading="paysLoading"
+          outlined
+          clearable
+          hide-details="auto"
+        >
         </v-combobox>
       </v-col>
-      <v-col cols="12" sm="6" md="3" pa-2>
-        <v-combobox v-model="selectedType" :items="types" label="Types" :loading="typeLoading" outlined ma-1>
+      <v-col cols="12" sm="6" md="3" class="pa-2">
+        <v-combobox
+          v-model="selectedType"
+          :items="types"
+          label="Types"
+          :loading="typeLoading"
+          outlined
+          clearable
+          hide-details="auto"
+        >
         </v-combobox>
       </v-col>
-      <v-col cols="12" sm="6" md="3" pa-2>
-        <v-text-field v-model="selectedName" label="Modèle" outlined />
+      <v-col cols="12" sm="6" md="3" class="pa-2">
+        <v-text-field v-model="selectedName" label="Modèle" outlined hide-details="auto" clearable />
       </v-col>
     </v-row>
-    <div>
-      <v-btn color="primary" class="float-right">Rechercher</v-btn>
-    </div>
+    <v-row no-gutters justify="end">
+      <v-btn color="primary" class="ma-2" @click.stop="search()">
+        <v-icon>mdi-magnify</v-icon>
+        Rechercher
+      </v-btn>
+    </v-row>
+    <v-row no-gutters class="px-2">
+      <v-data-table
+        :items="cars"
+        :loading="isLoading"
+        :headers="headers"
+        style="width: 100%"
+        :items-per-page="20"
+        hide-default-footer
+        :page.sync="page"
+        @page-count="pageCount = $event"
+      >
+      </v-data-table>
+    </v-row>
+    <v-row v-if="pageCount != 1" no-gutters justify="end" class="px-2">
+      <v-pagination v-model="page" :length="pageCount" total-visible="7"></v-pagination>
+    </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
 import { CarsApi } from '@/api/CarsApi';
+import { OriginalCarDTO } from '@/models/Cars/OriginalCarDTO';
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Concessionnaire extends Vue {
-  private makers = null;
+  private makers = [];
   private selectedMaker = '';
   private makerLoading = true;
 
-  private pays = null;
+  private pays = [];
   private selectedPays = '';
   private paysLoading = true;
 
-  private types = null;
+  private types = [];
   private selectedType = '';
   private typeLoading = true;
 
-  private selectedModel = '';
+  private selectedName = '';
+
+  private isLoading = false;
+  private cars: OriginalCarDTO[] = [];
+
+  private get headers() {
+    return [
+      {
+        text: 'Marque',
+        sortable: true,
+        value: 'maker',
+      },
+      {
+        text: 'Modèle',
+        sortable: true,
+        value: 'model',
+      },
+      {
+        text: 'Année',
+        sortable: true,
+        value: 'year',
+      },
+      {
+        text: 'Pays',
+        sortable: true,
+        value: 'origin',
+      },
+      {
+        text: 'Type',
+        sortable: true,
+        value: 'type',
+      },
+      {
+        text: 'Classe',
+        sortable: true,
+        value: 'pi',
+      },
+      {
+        text: 'Prix',
+        sortable: true,
+        value: 'price',
+      },
+    ];
+  }
+  private page = 1;
+  private pageCount = 0;
 
   mounted() {
     this.loadMakers();
     this.loadPays();
     this.loadTypes();
+
+    this.search();
   }
 
   async loadMakers() {
+    this.makerLoading = true;
     await CarsApi.getListMakers()
       .then((response: any) => {
         this.makers = response;
-        this.makerLoading = false;
       })
       .catch((err: any) => {
         console.log(err);
+      })
+      .finally(() => {
+        this.makerLoading = false;
       });
   }
 
   async loadPays() {
+    this.paysLoading = true;
     await CarsApi.getListPays()
       .then((response: any) => {
         this.pays = response;
-        this.paysLoading = false;
       })
       .catch((err: any) => {
         console.log(err);
+      })
+      .finally(() => {
+        this.paysLoading = false;
       });
   }
 
   async loadTypes() {
+    this.typeLoading = true;
     await CarsApi.getListTypes()
       .then((response: any) => {
         this.types = response;
-        this.typeLoading = false;
       })
       .catch((err: any) => {
         console.log(err);
+      })
+      .finally(() => {
+        this.typeLoading = false;
+      });
+  }
+
+  async search() {
+    this.isLoading = true;
+    await CarsApi.search(this.selectedMaker, this.selectedPays, this.selectedType, this.selectedName)
+      .then((reponse: any) => {
+        this.cars = reponse;
+      })
+      .catch((err: any) => {
+        console.log(err);
+      })
+      .finally(() => {
+        this.isLoading = false;
       });
   }
 }
